2e2e9904249f27f698d130f69c36e71609751d1b
Your GitHub Username <tannerhlyon@gmail.com>
2025-06-28 11:44:15 -0700

    feat: updated server operations

--- server/app.ts
+++ server/app.ts
@@ -2,19 +2,41 @@ import express, { ErrorRequestHandler } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
+// import { addReqId } from './logger.js';
+
+import { parseUserQuery } from './controllers/userQueryController.js';
+import {
+  queryOpenAIParse,
+  queryOpenAIEmbedding,
+  queryOpenAIChat,
+} from './controllers/openaiController.js';
+import { queryPineconeDatabase } from './controllers/pineconeController.js';
+import { queryByTitle } from './controllers/mongoController.js';
+// import { logQuery } from './controllers/loggingController.js';
+
 import { ServerError } from '../types/types.js';
 
 const app = express();
 
+// app.use(addReqId);
+
 app.use(cors());
 app.use(express.json());
 
-app.post('/api', (_req, res) => {
-  res.status(200).json({
-    movieRecommendation:
-      'Wishmaster - A malevolent genie wreaks havoc after being freed, leading to a battle between his dark desires and those trying to stop him.',
-  });
-});
+app.post(
+  '/api',
+  parseUserQuery,
+  queryOpenAIParse,
+  queryByTitle,
+  queryOpenAIEmbedding,
+  queryPineconeDatabase,
+  queryOpenAIChat,
+  (_req, res) => {
+    res.status(200).json({
+      movieRecommendation: res.locals.movieRecommendation,
+    });
+  }
+);
 
 const errorHandler: ErrorRequestHandler = (
   err: ServerError,
